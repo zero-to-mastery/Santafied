4b6099764dbef790d532b3ee97bffae0d3369f12
jianweilee128 <47060167+jianweilee128@users.noreply.github.com>
2019-10-06 03:48:44 +0800

    made changes to name-generator files

--- assets/js/elf-name-generator.js
+++ assets/js/elf-name-generator.js
@@ -1,38 +1,22 @@
-//array of first Name
+let choice = { firstletter: "Cookie", month: "Lord Crackle" };
 
-let firstNames = [ "Annabell", "Bella", "Bitty", "Bling-Bling", "BoBunny",
-  "Bubbles", "Button", "Charmy", "Cosmo", "Flower", "Fluffy", "Foxy",
-  "Frilly", "Glimmer", "Glitzy", "Happy", "Inky", "Jolly", "Jolly",
-  "Mischievous", "Moxie", "Perky", "Pixie", "Purdy", "Scrappy", "Shimmer",
-  "Shorty", "Snowy", "Sparkle", "Stampy", "Tinsel", "Tiny", "Twinkle", "Wonky"
-];
+//  letterChoice function
+function letterChoice() {
+  letter = document.getElementById("name").value;
+  choice.firstletter = letter;
+}
 
-//array of last Name
-
-let lastNames = [
-  "Big-ears", "Blue-bug", "Crafty-Pants", "Curly-Toes", "Gifty-Pants",
-  "Glitter-Bottom", "Jingle-Bells", "McGlitter", "McSprinkles", "Merry-Mess",
-  "Point-Ears", "Rolly-Renn", "Sugar-Gems", "Sugar-Stitches", "Twinkle-Toes",
-  "Twinkles", "Upset", "Yoyo", "Zoom"
-];
-
-//generateName function
-function generateName(){
-  let firstName =  firstNames[ Math.floor (Math.random() * firstNames.length ) ];
-  let lastName =  lastNames[ Math.floor (Math.random() * lastNames.length ) ];
-  return firstName + " " + lastName;
+//  monthChoice function
+function monthChoice() {
+  month = document.getElementById("month").value;
+  choice.month = month;
 }
 
 //printName function
-function printName(){
-
-  //get a random name
-  let theName = generateName();
-
-  //change the name
-  let nameContainer = document.querySelector('.name');
-  nameContainer.textContent = theName;
+function printName() {
+  let nameContainer = document.querySelector(".given-name");
+  nameContainer.textContent = choice.firstletter + " " + choice.month;
 }
 
 //the magic!
-document.getElementById('loadName').addEventListener("click", printName, false);
+document.getElementById("loadName").addEventListener("click", printName, false);
