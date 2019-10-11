ba140e8a62fa501bf79fbbd1311b9df71c534017
VrajKD <vrajkishan.dugar2017@vitstudent.ac.in>
2019-10-11 17:18:22 +0530

    Snowman generator uppercase fix

--- assets/js/snowman-name-generator.js
+++ assets/js/snowman-name-generator.js
@@ -8,19 +8,19 @@ function generateName() {
         result = document.getElementById('result');
 
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    
+
     const snowNames = ['Carrot', 'Eve', 'Sherry', 'Cozy', 'Teddy', 'Sweetie', 'Hope', 'Cookie', 'Robin', 'Tinsel', 'Joy', 'Sugarplum', 'Sparkle', 'Magic', 'Candy', 'Festive', 'Berry', 'Holly', 'Rosie', 'Twinkle', 'Angel', 'Frostie', 'Bells', 'Merry', 'Jingle', 'Joy'];
 
     const snowSurnames = ['McSnowflake', 'McSparkles', 'McBlizzard', 'McIcicles', 'McSnowballs', 'McFreeze', 'McFrosty', 'McSparkle', 'McGlisten', 'McChilly', 'McSlushy', 'McSnowy'];
 
     if (firstLetter && userMonth) {
-        index = alphabet.indexOf(firstLetter);
+        index = alphabet.indexOf(firstLetter.toLowerCase());
         snowmanName = snowNames[index];
         snowmanSurname = snowSurnames[userMonth];
         document.getElementById("resultContainer").style.display = "block";
         result.textContent = snowmanName + ' ' + snowmanSurname;
     } else {
         alert('Please input your data first.');
-    } 
+    }
 
 }
