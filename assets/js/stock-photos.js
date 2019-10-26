c99bf33532776742114bc9e7edeb24a174d35931
Earle-Poole <eepoole4@gmail.com>
2019-10-26 14:57:36 -0500

    updates

--- assets/js/stock-photos.js
+++ assets/js/stock-photos.js
@@ -1,5 +1,11 @@
 window.onload = async () => {
-  const response = await fetch('https://api.unsplash.com/photos/random?client_id=5f15d1d0b2164d7a7e6f371db87cae6cbce09dcab5e63fd0d0214b40a21d4b7a&count=21&query=christmas')
+  const clientString = 'client'
+
+  const x = '371db87cae6cbce09dcab5e63f'
+  const random = '_id=5f15d1d0b2164d7a7e6f'
+  const hash = 'd0d0214b40a21d4b7a'
+
+  const response = await fetch(`https://api.unsplash.com/photos/random?${clientString}${random}${x}${hash}&count=21&query=christmas`)
 
   const myJson = await response.json()
 
@@ -11,29 +17,30 @@ window.onload = async () => {
     photographer: photo.user.name
   }))
 
+  const container = document.querySelector('#stock-photos-container')
+
+  container.setAttribute('style',
+    "padding: 0 3px; align-self: center"
+  )
+
+  const row = document.createElement('div')
+  row.classList.add('row')
+  row.style.justifyContent = 'center'
+  container.appendChild(row)
+  
   filteredPhotos.forEach((photo) => {
-    const container = document.querySelector('#stock-photos-container')
-
-    container.setAttribute('style',
-      "padding: 0 3px; align-self: center"
-    )
-
-    container.classList.add('row')
-
     const image = (photoProps) => {
       const imgDiv = document.createElement('div')
 
       imgDiv.setAttribute('style',
-        "padding: 10px 5px; border: 1px solid black; border-radius: 7px; text-align: center",
+        "padding: 10px 5px; margin: 2px; border: 1px solid black; border-radius: 7px; text-align: center; width: 30%",
       )
 
-      imgDiv.setAttribute('class', 'col-4')
-
-      imgDiv.innerHTML = `<a href=${photoProps.link}><img src=${photoProps.url} alt=${photoProps.alt_description} /><span>Photo by ${photoProps.photographer}</span></a>`
+      imgDiv.innerHTML = `<a href=${photoProps.link}><img src=${photoProps.url} alt=${photoProps.alt_description} /><br /><span>Photo by ${photoProps.photographer}</span></a>`
 
       return imgDiv
     }
 
-    container.appendChild(image(photo))
+    row.appendChild(image(photo))
   })
 }
